e0b4d3dec982041588ef1160e5227231fdcd4212
Paweł Stawikowski <pawikoski@gmail.com>
2024-10-03 15:02:44 +0200

    Handle action to fetch comments from API

--- src/background.js
+++ src/background.js
@@ -6,16 +6,21 @@
 // See https://developer.chrome.com/extensions/background_pages
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
+  if (request.action === "fetchCommentsHTML") {
+    fetch('http://localhost:8000/api/comments')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(htmlContent => {
+        sendResponse({ html: htmlContent });
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+        sendResponse({ error: error.message });
+      });
+    return true; // Keep the message channel open for sendResponse
   }
 });
